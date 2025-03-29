1aba1c5d5166388225c5112b7174838c4d96bfa6
satyamrao-0001 <satyam14781@gmail.com>
2025-03-29 23:53:34 +0530

    while loop  for count of no

--- first.cpp
+++ first.cpp
@@ -6,17 +6,31 @@
 // cout<<"Hello World"; 
 // }
 
+//  #include<iostream>
+// using namespace std ;
+// int main (){
+//     int age ;
+//     cout<<"Enter age : ";
+//     cin>>age ;
+//     if (age>=18){
+//         cout<<"can give the vote\n";
+//     } 
+//     else {
+//         cout<<"can't give the vote \n";
+//     }
+//     return 0 ;
+// }
+
 #include<iostream>
 using namespace std ;
 int main (){
-    int age ;
-    cout<<"Enter age : ";
-    cin>>age ;
-    if (age>=18){
-        cout<<"can give the vote\n";
-    } 
-    else {
-        cout<<"can't give the vote \n";
-    }
-    return 0 ;
+int count = 1 ;
+while (count < 10
+) {
+    cout<<count<<" ";
+    count++;
+    
+
+}
+return 0 ;
 }
