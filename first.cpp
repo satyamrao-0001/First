d74fedd457f715e9ebeee774e93e2f6516e49fbf
satyamrao-0001 <satyam14781@gmail.com>
2025-03-30 22:11:55 +0530

    for loop for oddsum

--- first.cpp
+++ first.cpp
@@ -21,16 +21,65 @@
 //     return 0 ;
 // }
 
+// #include<iostream>
+// using namespace std ;
+// int main (){
+// int count = 1 ;
+// while (count < 10
+// ) {
+//     cout<<count<<" ";
+//     count++;
+    
+
+// }
+// return 0 ;
+// }
+// #include<iostream>
+// using namespace std ; 
+
+// void printarr(int arr[], int n){
+//     for(int i=0 ; i<n ; i++) {
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl; 
+// }
+
+// void selectionsort(int arr[], int n) {
+
+// for(int i=0 ; i<n-1 ; i++) {
+//     int smallestidx = i ;
+//     for(int j=i+1 ; j<n ; j++){
+
+//         if(arr[j]<arr[ smallestidx]) {
+//             smallestidx =j ; 
+          
+//         }
+//     }
+//     swap( arr[i] , arr[smallestidx] ) ;
+// }
+// }
+// int main (){
+//     int n=5 ; 
+//     int arr[]={4,1,5,2,3} ; 
+
+//     selectionsort(arr,n) ;
+
+//     printarr(arr,n);
+    
+
+// }
 #include<iostream>
 using namespace std ;
 int main (){
-int count = 1 ;
-while (count < 10
-) {
-    cout<<count<<" ";
-    count++;
+int n = 10 ;
+int oddsum = 0 ;
+for ( int i=1 ; i<=10 ; i++) {
     
+    if(i%2 !=0) {
+        oddsum+=i ; }
+        
+    }
+    cout<<"oddsum ="<<oddsum<<endl;
+    return 0;
 
-}
-return 0 ;
 }
