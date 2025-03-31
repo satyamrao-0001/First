a47096aa860ddcefdadf47d38d7f66da8b31221a
satyamrao-0001 <satyam14781@gmail.com>
2025-03-31 21:41:15 +0530

    pattern

--- first.cpp
+++ first.cpp
@@ -68,18 +68,31 @@
     
 
 // }
-#include<iostream>
-using namespace std ;
-int main (){
-int n = 10 ;
-int oddsum = 0 ;
-for ( int i=1 ; i<=10 ; i++) {
+// #include<iostream>
+// using namespace std ;
+// int main (){
+// int n = 10 ;
+// int oddsum = 0 ;
+// for ( int i=1 ; i<=10 ; i++) {
     
-    if(i%2 !=0) {
-        oddsum+=i ; }
+//     if(i%2 !=0) {
+//         oddsum+=i ; }
         
-    }
-    cout<<"oddsum ="<<oddsum<<endl;
-    return 0;
+//     }
+//     cout<<"oddsum ="<<oddsum<<endl;
+//     return 0;
 
-}
+// }
+
+
+
+
+#include <iostream>
+ using namespace std ; 
+ int main () {
+    int n=5 ; 
+    for(int i=1 ; i<=n  ;  i++ ) {
+        cout<<" *****"<<endl;
+    }
+
+ }
