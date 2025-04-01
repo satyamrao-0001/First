111f847af329d763fd60a9664754d4c8fba69189
satyamrao-0001 <satyam14781@gmail.com>
2025-04-01 23:49:02 +0530

    pattern 1234

--- first.cpp
+++ first.cpp
@@ -87,12 +87,24 @@
 
 
 
-#include <iostream>
- using namespace std ; 
- int main () {
-    int n=5 ; 
-    for(int i=1 ; i<=n  ;  i++ ) {
-        cout<<" *****"<<endl;
-    }
+// #include <iostream>
+//  using namespace std ; 
+//  int main () {
+//     int n=5 ; 
+//     for(int i=1 ; i<=n  ;  i++ ) {
+//         cout<<" *****"<<endl;
+//     }
 
- }
+#include<iostream>
+using namespace std ; 
+int main () {
+    int n=4 ;
+    for(int i=1 ; i<=n ; i++ ) {
+
+        for(int j=1 ; j<=4 ; j++) {
+            cout<<j ;
+
+        }
+        cout<<endl; 
+    }
+}
