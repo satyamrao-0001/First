b7acc99b6ee6e8302aec7ac1808b11dd8916c091
satyamrao-0001 <satyam14781@gmail.com>
2025-04-03 22:49:33 +0530

    pattern fot numbers

--- first.cpp
+++ first.cpp
@@ -108,6 +108,18 @@
 //         cout<<endl; 
 //     }
 // }
+// #include<iostream>
+// using namespace std ; 
+// int main () {
+//     int n = 5;
+//     for(int i=0 ; i<n ; i++) {
+
+//         for(int j=0; j<i+1 ; j++ ){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
 #include<iostream>
 using namespace std ; 
 int main () {
@@ -115,8 +127,9 @@ int main () {
     for(int i=0 ; i<n ; i++) {
 
         for(int j=0; j<i+1 ; j++ ){
-            cout<<"*";
+            cout<<i+1;
         }
         cout<<endl;
     }
+
 }
