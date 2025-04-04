9b5094c002d17891770f511a4038b8d24e58f1e1
satyamrao-0001 <satyam14781@gmail.com>
2025-04-04 21:56:47 +0530

    pattern

--- first.cpp
+++ first.cpp
@@ -120,16 +120,35 @@
 //         cout<<endl;
 //     }
 // }
+// #include<iostream>
+// using namespace std ; 
+// int main () {
+//     int n = 5;
+//     for(int i=0 ; i<n ; i++) {
+
+//         for(int j=0; j<i+1 ; j++ ){
+//             cout<<i+1;
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
 #include<iostream>
 using namespace std ; 
-int main () {
-    int n = 5;
-    for(int i=0 ; i<n ; i++) {
+int main (){
+    int n = 5 ; 
+    int num= 1;
+for(int i= 0 ; i<n ; i++ ) {
+        for( int j=i+1 ; j>0 ; j-- ){
 
-        for(int j=0; j<i+1 ; j++ ){
-            cout<<i+1;
+            cout<<num<<"  " ;
+            num=num +1 ;
+
         }
-        cout<<endl;
+        cout<<endl; 
+
+    }
     }
 
-}
+
