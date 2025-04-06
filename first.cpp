a818451b8f997dc74fb7180876a9027bbc601dca
satyamrao-0001 <satyam14781@gmail.com>
2025-04-06 18:04:03 +0530

    vector

--- first.cpp
+++ first.cpp
@@ -151,22 +151,33 @@
 //     }
 //     }
 
-#include<iostream>//VECTORS
+// #include<iostream>//VECTORS
+// #include<vector>
+// using namespace std ;
+
+// int main (){
+//   int array[5]={1,2,3,4,5} ;
+//   int n = 5 ; 
+//   for(int st=0 ; st<n ; st ++){
+//       for(int end=st ; end<n ; end ++){
+//         for(int i=st ; i<=end ; i++ ){
+//           cout<<array[i] ;
+//         }
+
+//      cout<<" " ;  
+//   }
+  
+//  cout<<endl;
+// }
+// }
+
+#include<iostream>
 #include<vector>
 using namespace std ;
 
-int main (){
-  int array[5]={1,2,3,4,5} ;
-  int n = 5 ; 
-  for(int st=0 ; st<n ; st ++){
-      for(int end=st ; end<n ; end ++){
-        for(int i=st ; i<=end ; i++ ){
-          cout<<array[i] ;
-        }
-
-     cout<<" " ;  
+int main () {
+  vector<char>vec={'a','b','c'} ;
+  for(char val : vec ){
+    cout<<val<<endl; 
   }
-  
- cout<<endl;
-}
 }
